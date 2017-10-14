47b7d3a4b56e157ce4f590fc5c97d72c8bf746c4
andrei_fedorov <afedorov@luc.edu>
2017-10-14 11:27:04 -0500

    initial project files

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StockTrading.Startup))]
+namespace StockTrading
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
